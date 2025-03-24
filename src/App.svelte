09dc1a723ebea6f3f66d06d5e79392ab4f3d0640
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-24 15:04:16 +0700

    complete discount section

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import Navbar from "./lib/components/Navbar.svelte";
     import BlogPost from "./lib/sections/BlogPost.svelte";
+  import Discount from "./lib/sections/Discount.svelte";
     import Hero from "./lib/sections/Hero.svelte";
     import Latest from "./lib/sections/Latest.svelte";
     import Testimonial from "./lib/sections/Testimonial.svelte";
@@ -15,6 +16,7 @@
         <Latest latestItems={data.latest} />
         <Testimonial testimonials={data.testimonials} />
         <BlogPost blogPosts={data.blogs} />
+        <Discount />
     </main>
 </div>
 
